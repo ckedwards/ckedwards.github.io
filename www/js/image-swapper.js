741cd9d8933aecd89ab070e8380ea1097d7aa365
Christopher Edwards <Christopher.Edwards@sas.com>
2020-03-27 16:47:21 -0400

    test ie fix

--- www/js/image-swapper.js
+++ www/js/image-swapper.js
@@ -2,22 +2,20 @@
   var styleEl = document.createElement('style');
   document.head.appendChild(styleEl);
   var styleSheet = styleEl.sheet;
-  for (const rule of ['image-swapper {display: block;}']) {
-    styleSheet.insertRule(rule);
-  }
+  styleSheet.insertRule('image-swapper {display: block;}');
 
   function getImageData(images) {
     if (!images) {
       return [];
     }
-    const allImages = images.split(',').map((img) => img.trim());
-    const processed = [];
-    for (const img of allImages) {
+    var allImages = images.split(',').map((img) => img.trim());
+    var processed = [];
+    allImages.forEach(function(img) {
       processed.push({
         width: parseInt(img.substring(img.lastIndexOf('-') + 1, img.lastIndexOf('x'))),
         src: img,
       });
-    }
+    });
     processed.sort((a, b) => b.width - a.width);
     return processed;
   }
@@ -43,7 +41,7 @@
         this._img.style.height = '100%';
         this._outer.appendChild(this._img);
 
-        const resizeObserver = new ResizeObserver((entries) => {
+        var resizeObserver = new ResizeObserver((entries) => {
           this._observedWidth = entries[0].contentRect.width;
           this._getClosestImage();
         });
@@ -54,7 +52,7 @@
         if (this._reportContentComplete) {
           return;
         }
-        const width = this._observedWidth;
+        var width = this._observedWidth;
         if (!this.images.length || !width) {
           return;
         }
@@ -62,7 +60,7 @@
           this._img.setAttribute('src', this.images[0].src);
           return;
         }
-        const index = this.images.findIndex((val) => width >= val.width);
+        var index = this.images.findIndex((val) => width >= val.width);
         // First item width is smaller than observed width
         if (index === 0) {
           this._img.setAttribute('src', this.images[0].src);
@@ -73,8 +71,8 @@
           this._img.setAttribute('src', this.images[this.images.length - 1].src);
           return;
         }
-        const smaller = this.images[index];
-        const larger = this.images[index - 1];
+        var smaller = this.images[index];
+        var larger = this.images[index - 1];
         if (width - smaller.width < larger.width - width) {
           this._img.setAttribute('src', smaller.src);
         } else {
